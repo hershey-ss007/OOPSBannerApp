9334412b41ea5a03a9af70c01bc2c46ecf944541
hershey-ss007 <hm4585@srmist.edu.in>
2026-02-20 21:28:06 +0530

    Coded to print OOPS to console

--- OOPSBannerAp.java
+++ OOPSBannerAp.java
@@ -1,54 +1,93 @@
-public class OOPSBannerAp{
+public class OOPSBannerAp {
 
-    
-    public static String[] getOPattern() {
+    /**
+     * Inner static class to encapsulate character and its banner pattern
+     */
+    public static class CharacterPattern {
+        private char character;
+        private String[] pattern;
+
+        /**
+         * Constructor to initialize character and its pattern
+         * @param character The character being represented
+         * @param pattern The 7-line banner pattern for the character
+         */
+        public CharacterPattern(char character, String[] pattern) {
+            this.character = character;
+            this.pattern = pattern;
+        }
+
+        /**
+         * Getter for character
+         * @return the character
+         */
+        public char getCharacter() {
+            return character;
+        }
+
+        /**
+         * Getter for pattern
+         * @return the banner pattern array
+         */
+        public String[] getPattern() {
+            return pattern;
+        }
+    }
+
+    // Utility methods to build patterns for O, P, S
+    public static String[] buildOPattern() {
         return new String[] {
-            "  ***  ",
-            " ** ** ",
-            "**   **",
-            "**   **",
-            "**   **",
-            " ** ** ",
-            "  ***  "
+            "  *****  ",
+            " *     * ",
+            " *     * ",
+            " *     * ",
+            " *     * ",
+            " *     * ",
+            "  *****  "
         };
     }
 
-    
-    public static String[] getPPattern() {
+    public static String[] buildPPattern() {
         return new String[] {
-            "****** ",
-            "**  ** ",
-            "**  ** ",
-            "****** ",
-            "**     ",
-            "**     ",
-            "**     "
+            " *****   ",
+            " *    *  ",
+            " *    *  ",
+            " *****   ",
+            " *       ",
+            " *       ",
+            " *       "
         };
     }
 
-
-    public static String[] getSPattern() {
+    public static String[] buildSPattern() {
         return new String[] {
-            " *****",
-            "**    ",
-            "**    ",
-            " *****",
-            "    **",
-            "    **",
-            " *****"
+            "  *****  ",
+            " *       ",
+            " *       ",
+            "  *****  ",
+            "       * ",
+            "       * ",
+            "  *****  "
         };
     }
 
-    
     public static void main(String[] args) {
+        // Create CharacterPattern objects
+        CharacterPattern o1 = new CharacterPattern('O', buildOPattern());
+        CharacterPattern o2 = new CharacterPattern('O', buildOPattern());
+        CharacterPattern p  = new CharacterPattern('P', buildPPattern());
+        CharacterPattern s  = new CharacterPattern('S', buildSPattern());
 
-        
-        String[] oPattern = getOPattern();
-        String[] pPattern = getPPattern();
-        String[] sPattern = getSPattern();
+        // Array of CharacterPattern objects
+        CharacterPattern[] word = { o1, o2, p, s };
 
-        for (int i = 0; i < oPattern.length; i++) {
-            System.out.println(oPattern[i] + "  " + oPattern[i] + "  " + pPattern[i] + "  " + sPattern[i]);
+        // Print banner line by line
+        for (int i = 0; i < 7; i++) {
+            StringBuilder line = new StringBuilder();
+            for (CharacterPattern cp : word) {
+                line.append(cp.getPattern()[i]);
+            }
+            System.out.println(line.toString());
         }
     }
 }
